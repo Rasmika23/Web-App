88f48ea0a66cedccf145b4938ecc267cc9f28fcd
Rasmika23 <rasmikanammunige23@gmail.com>
2025-07-15 18:02:21 +0530

    Frontend until explore menu feature

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the fines ingredients and</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
