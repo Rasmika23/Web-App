b486bb34ff7a4eec7985f939dc5a0845f47096bb
Rasmika23 <rasmikanammunige23@gmail.com>
2025-07-22 18:03:22 +0530

    Completed front end part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import { PlaceOrder } from './pages/PlaceOrder/PlaceOrder'
+import { Footer } from './components/Footer/Footer'
 
-const App = ()=>{
-  return(
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/cart' element={<Home/>}></Route> {/* SHOULD CHANGE THIS */}
-        <Route path='/order' element={<PlaceOrder/>}></Route>
-      </Routes>
-    </div>
+const App = () => {
+  return (
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/cart' element={<Home />}></Route> {/* SHOULD CHANGE THIS */}
+          <Route path='/order' element={<PlaceOrder />}></Route>
+        </Routes>
+      </div>
+      <Footer />
+    </>
+
   )
 }
 
