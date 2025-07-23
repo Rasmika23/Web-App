60f5db22a6bab8b8847ce9d28780f284489e0728
Rasmika23 <rasmikanammunige23@gmail.com>
2025-07-23 19:46:26 +0530

    Login feature added-working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,17 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import { PlaceOrder } from './pages/PlaceOrder/PlaceOrder'
 import { Footer } from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false);
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/cart' element={<Home />}></Route> {/* SHOULD CHANGE THIS */}
