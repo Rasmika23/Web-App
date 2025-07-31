bc50d08c167b552c04f4592fe38ae2c1e12c1f36
Rasmika23 <rasmikanammunige23@gmail.com>
2025-07-31 19:22:08 +0530

    Done User authentication and working

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken"   // For Authentication
+import bycryt from "bcrypt"   // For password encryption
+import validator from "validator"
+import { response } from "express";
+
+
+//Login user
+
+const loginUser = async(req ,res)=>{
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false,message:"User doesn't exist"})
+        }
+        const isMatch = await bycryt.compare(password, user.password)
+
+        if (!isMatch) {
+            return res.json({success:false, message:"Invalid credentials"})
+        }
+
+        const token = createToken(user._id);
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+const createToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
+//Register user
+
+const registerUser = async(req ,res)=>{
+    const {name,password,email} = req.body;
+    try {
+        //Checking is user already exists
+        const exists = await userModel.findOne({email});
+        if (exists) {
+            return res.json({success:false,message:"User already exists for this mail"})
+        }
+
+        // Validating email format & strong password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please enter a valid email"})
+        }
+
+        if (password.length<8) {
+            return res.json({success:false, message:"Please enter a strong password"})
+        }
+
+        // Hashing user password
+        const salt = await bycryt.genSalt(10);
+        const hashedPassword = await bycryt.hash(password,salt);
+
+        const newUser = new userModel({
+            name:name,
+            email:email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save()
+        const token = createToken(user._id)
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+
+
+export {loginUser, registerUser}
--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,14 +1,56 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets'
+import { StoreContext } from '../../context/StoreContex'
+import axios from "axios"
 
 const LoginPopup = ({setShowLogin}) => {
+
+    const {url, setToken} = useContext(StoreContext);
+
     const [currState, setCurrState] = useState("Signup")
+
+    const [data,setData] = useState({
+        name:"",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+    }
+
+    const onLogin = async(event) =>{
+        event.preventDefault()
+        let newUrl = url;
+        if (currState==="Login") {
+            newUrl += "/api/user/login"
+        }
+        else{
+            newUrl += "/api/user/register"
+        }
+
+        const response = await axios.post(newUrl,data);
+
+        if (response.data.success) {
+            setToken(response.data.token);
+            localStorage.setItem("token",response.data.token);
+            setShowLogin(false)
+        }
+        else{
+            alert(response.data.message)
+        }
+
+    }
+
+
 
 
   return (
     <div className='login-popup'>
-        <form className='login-popup-container'>
+        <form onSubmit={onLogin} className='login-popup-container'>
             <div className='login-popup-title'>
                 <h2>{currState}</h2>
                 <img onClick={() =>setShowLogin(false)} src={assets.cross_icon} />
@@ -17,17 +59,17 @@ const LoginPopup = ({setShowLogin}) => {
 
             <div className='login-popup-input'>
                 {currState==="Login"?<></>:
-                    <input type = "text" placeholder='Your name' required></input>
+                    <input name='name' onChange={onChangeHandler} value={data.name} type = "text" placeholder='Your name' required></input>
                 }
                 
-                <input type = "email" placeholder='Your email' required></input>
-                <input type = "password" placeholder='Your password' required></input>
+                <input name='email' onChange={onChangeHandler} value={data.email} type = "email" placeholder='Your email' required></input>
+                <input name='password' onChange={onChangeHandler} value={data.password} type = "password" placeholder='Your password' required></input>
 
 
                 
             </div>
 
-            <button>{currState==="Signup"?"Create account":"Login"}</button>
+            <button type='submit'>{currState==="Signup"?"Create account":"Login"}</button>
 
             <div className='login-popup-condition'>
                 <input type="checkbox" required></input>
