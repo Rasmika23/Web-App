ad619618805dab75f29843c4f513017f8cb9d38e
Rasmika23 <rasmikanammunige23@gmail.com>
2025-08-01 11:09:43 +0530

    Done cart functionality and working

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 
 //app config
-const app = express()
+const app = express();
 
 const port = 4000
 
 //middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors())
 
 //db connection
@@ -22,6 +23,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
