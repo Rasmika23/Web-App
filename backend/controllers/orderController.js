f09679dbee44171eed8ef086d85abddab29fedb0
heyiamheshan <rmheshanpramuditha@gmail.com>
2025-08-02 11:34:32 +0530

    Complete admin panel orders section

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,5 +86,28 @@ const userOrders = async (req,res) => {
         res.json({success:false,message:"Error"});
     }
 }
+// listing orders for admin panel
 
-export {placeOrder, verifyOrder, userOrders}
+const listOrders=async(req,res)=>{
+  try {
+    const orders =await orderModel.find({});
+    res.json({success:true,data :orders })
+  } catch (error) {
+    console.log(error);
+    res.json({success:fals,message:"Error"})
+  }
+
+
+}
+// api for updating order status 
+const updateStatus=async(req,res)=>{
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders,listOrders,updateStatus}
