a2a767cbd1f8820bdbcd5eb81aa5f8905346923e
Rasmika Nammunige <164039671+Rasmika23@users.noreply.github.com>
2025-08-02 20:21:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin : 'https://tastio-frontend.netlify.app/',
+    origin : 'https://tastio-frontend.netlify.app',
     credentials: true
 }))
 
