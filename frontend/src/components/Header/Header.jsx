3f9141085c7ed8823268f6d1e3e29203dcd09ecd
heyiamheshan <rmheshanpramuditha@gmail.com>
2025-08-02 16:40:46 +0530

    Change navigation bar and add video to background

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
 
 export const Header = () => {
   return (
-    <div className='header'>
-        <div className="header-content">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the fines ingredients and</p>
-            <button>View Menu</button>
-        </div>
+    <div className="header">
+      <video className="bg-video" autoPlay muted loop playsInline>
+        <source src="/header_video2.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients and
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
