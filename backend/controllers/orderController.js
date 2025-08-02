d9990267682db56b904e5bca21c11fb874c00137
Rasmika23 <rasmikanammunige23@gmail.com>
2025-08-02 10:39:15 +0530

    All done and working

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //Placing order for frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "http://localhost:5173"
+    const frontend_url = "http://localhost:5174"
     
     try {
         const newOrder = new orderModel({
@@ -87,4 +87,26 @@ const userOrders = async (req,res) => {
     }
 }
 
-export {placeOrder, verifyOrder, userOrders}
+// Listing orders for admin panel
+const listOrder = async (req,res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error){
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+//Api for updating order status
+const updateStatus = async (req,res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders, listOrder,updateStatus}
