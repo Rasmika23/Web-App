d43a0541585174661e445171ee1b31cdcab2a9fb
HasiruChamodya <commenty460@gmail.com>
2025-08-03 13:36:00 +0530

    frontend changes currency

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,7 @@ const placeOrder = async (req,res) => {
                 product_data:{
                     name:"Delivery Charges"
                 },
-                unit_amount:2*100
+                unit_amount:150*100
             },
             quantity:1
         })
