fca8ca554afd4ff78e56400440ee70335526474d
heyiamheshan <rmheshanpramuditha@gmail.com>
2025-08-03 10:18:53 +0530

    Add video to frontend background

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+ import React from 'react'
 import './Header.css'
 
 export const Header = () => {
   return (
     <div className='header'>
-        <div className="header-content">
-            <h2>Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the fines ingredients and</p>
-            <button>View Menu</button>
-        </div>
+      <video
+        className="header-video"
+        autoPlay
+        muted
+        loop
+        playsInline
+      >
+        <source src="/yo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu featuring a delectable array of dishes
+          crafted with the finest ingredients.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
   )
 }
